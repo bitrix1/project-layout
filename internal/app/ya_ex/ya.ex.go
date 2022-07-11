59308427dfbbdc93672bf10cfa15ec594949ab26
bitrix1 <icq33455286@gmail.com>
2022-07-11 18:49:15 +0300

    ya_ex B. Кольцевая линия метро

--- internal/app/ya_ex/ya.ex.go
+++ internal/app/ya_ex/ya.ex.go
@@ -1,22 +1,108 @@
 package main
 
-//import "fmt"
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
-	//"strings"
+	"strings"
+	//"sort"
 )
 
 func main() {
+	arr := []int{}
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.Trim(text, "\r\n")
+	//text = strings.Replace(string(text), "\n", "", -1)
+	s := strings.Split(text, " ")
+	for i := range s {
+		e, _ := strconv.Atoi(s[i])
+		arr = append(arr, e)
+	}
+
+	//fmt.Println(arr[0])
+	d := arr[0]
+	m := arr[1]
+	y := arr[2]
+	y = y
+	//   12 13 1970     13 12 2004
+	if (d <= 12 && m > 12) || (d > 12 && m <= 12) /*&& y>=1970&&y<=2069*/ {
+		fmt.Println(1)
+		// fmt.Print(n, i, j)
+		//fmt.Print(a)
+	} else {
+		// fmt.Println("else")
+		// fmt.Print(n, i, j)
+		fmt.Print(0)
+
+	}
+}
+
+func main_C() {
+	// return errors.New("empty name")
+	// y := math.Abs(math.Inf(-11))
+	// fmt.Print(y)
+	// x := int64(-2)
+	// fmt.Println(i64.Abs(x))
+	arr := []int{}
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.Trim(text, "\r\n")
+	fmt.Println(len(text))
+	return
+	// text = strings.Replace(string(text), "\r\n", "", -1)
+	s := strings.Split(text, " ")
+	for i := range s {
+		e, _ := strconv.Atoi(s[i])
+		arr = append(arr, e)
+	}
+
+	// fmt.Println(arr[0])
+	sort.Ints(arr)
+	// fmt.Println(arr[2])
+
+	n := arr[2]
+	i := arr[0]
+	j := arr[1]
+	fmt.Print(n, i, j)
+
+	a := j - i - 1
+	b := n - j + i - 1
+	// if a < 0 {
+	// 	a *= -1
+	// }
+	// if b < 0 {
+	// 	b *= -1
+	// }
+	// a -= 1
+	// b -= 1
+	if (a) < (b) {
+		// fmt.Println("if")
+		// fmt.Print(n, i, j)
+		fmt.Print(a)
+	} else {
+		// fmt.Println("else")
+		// fmt.Print(n, i, j)
+		fmt.Print(b)
+
+	}
+	// for i := range s {
+	// }
+	// for i:=0;i<3;i++ {
+	// }
+}
+func main_A() {
 	reader := bufio.NewReader(os.Stdin)
 	arr := make([]int, 3)
 	for i := range arr {
 		text, _ := reader.ReadString('\n')
 		// convert CRLF to LF
-		//text= strings.Replace(text, "\n", "", -1)
-		arr[i], _ = strconv.Atoi(text)
+		text = strings.Replace(text, "\r\n", "", -1)
+		a, _ := strconv.Atoi(text)
+		arr[i] = a
+		//fmt.Print(arr, ok)
 		//if strings.Compare("hi", text) == 0 {
 		//  fmt.Println("hello, Yourself")
 		//}
@@ -24,28 +110,28 @@ func main() {
 		//fmt.Println(m)
 	}
 	//for i:= range arr {
-	//fmt.Fprintln(os.Stdout, arr[i])
+	//fmt.Print(arr[i])
 	//}
 	if arr[1] == 0 {
 		if arr[0] != 0 {
-			fmt.Fprintln(os.Stdout, 3)
+			fmt.Print(3)
 		} else {
-			fmt.Fprintln(os.Stdout, arr[2])
+			fmt.Print(arr[2])
 		}
 	} else if arr[1] == 1 {
-		fmt.Fprintln(os.Stdout, arr[2])
+		fmt.Print(arr[2])
 	} else if arr[1] == 4 {
 		if arr[0] == 0 {
-			fmt.Fprintln(os.Stdout, 4)
+			fmt.Print(4)
 		} else {
-			fmt.Fprintln(os.Stdout, 3)
+			fmt.Print(3)
 		}
 
 	} else if arr[1] == 6 {
-		fmt.Fprintln(os.Stdout, 0)
+		fmt.Print(0)
 	} else if arr[1] == 7 {
-		fmt.Fprintln(os.Stdout, 1)
+		fmt.Print(1)
 	} else {
-		fmt.Fprintln(os.Stdout, arr[1])
+		fmt.Print(arr[1])
 	}
 }
